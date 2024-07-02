8a174a3eb2632c5e6725d100f6f03833ea304259
Raka <rakapa23@gmail.com>
2024-07-02 23:42:09 +0800

    push

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# tugas-docker
+
+# Praktikum Docker
+
+NAMA : RAKA PUTRA ATMAJA
+
+NIM : 2215101010
+
+KELAS : A
+
+
+
+
+## Docker Container
+![App Screenshot](https://i.ibb.co.com/RStDr27/image.png)
+
+## Hasil Web
+![App Screenshot](https://i.ibb.co.com/L1h1ccH/image.png)
+
---
+++ web/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+        .card {
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            padding: 20px;
+            max-width: 400px;
+            text-align: center;
+        }
+        .card h2 {
+            margin: 10px 0;
+            color: #333;
+        }
+        .card p {
+            margin: 5px 0;
+            color: #777;
+        }
+    </style>
+</head>
+<body>
+<div class="card">
+    <h2>RAKA PUTRA ATMAJA</h2>
+    <p>NIM: 2215101010</p>
+    <p>KELAS: A</p>
+</div>
+</body>
+</html>
